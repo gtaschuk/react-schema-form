5add364c8c040389b394fb8e90d0c6ea6cfc941b
D.V.D <entropyhacker@gmail.com>
2018-08-06 16:58:16 +0300

    Checkbox: fixed label and handler

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -1,49 +1,41 @@
-/**
- * Created by steve on 20/09/15.
- */
-import React from 'react';
+import React, {Component} from 'react';
 import ComposedComponent from './ComposedComponent';
 import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
-const styles = {
-    error: {
-        color: 'rgb(244, 67, 54)',
-        fontSize: '12px',
-        lineHeight: '12px',
-    },
-    checkbox: {
-        marginTop: '14px',
-        marginBottom: '0px',
-        height: '72px',
-    },
-};
-
-class Checkbox2 extends React.Component {
-
+class FormCheckbox extends Component {
     constructor (props) {
         super(props);
         const {model, form} = this.props;
         const {key} = form;
         //If a boolean is stored, use it; if not, if a boolean is defined as schema's default, use it.
-        const value = _.isBoolean(this.props.value)? this.props.value : (_.isBoolean(form.schema.default)? form.schema.default : undefined);
+        const value = typeof this.props.value === 'boolean'
+            ? this.props.value
+            : typeof form.schema.default === 'boolean'
+                ? form.schema.default
+                : undefined
         this.props.setDefault(key, model, form, value)
     }
+    handleChange = e => {
+        this.props.onChangeValidate(e);
+    };
 
     render() {
         return (
-            <div className={this.props.form.className} style={styles.checkbox}>
-                <Checkbox
+            <FormControlLabel
+                className={this.props.form.className}
+                label={this.props.form.title}
+                control={
+                    <Checkbox
                     name={this.props.form.key.slice(-1)[0]}
                     value={this.props.form.key.slice(-1)[0]}
-                    checked={this.props.value}
-                    label={this.props.form.title}
+                    checked={this.props.value || false}
                     disabled={this.props.form.readonly}
-                    onCheck={(e, checked) => {this.props.onChangeValidate(e)}}
-                    />
-                <span style={styles.error}>{this.props.errorText}</span>
-             </div>
+                    onChange={this.handleChange} />
+                }
+            />
         );
     }
 }
 
-export default ComposedComponent(Checkbox2);
+export default ComposedComponent(FormCheckbox);
