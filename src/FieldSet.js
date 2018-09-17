691da67700bd4815d550fb8b9db414da69e80890
D.V.D <entropyhacker@gmail.com>
2018-09-17 17:26:05 +0300

    Array: remove duplicated labels from Array elements

--- src/FieldSet.js
+++ src/FieldSet.js
@@ -20,7 +20,6 @@ class FieldSet extends React.Component {
 
         return (
             <FormControl component="fieldset" className={classes.root}>
-                <FormLabel component="legend">{form.title}</FormLabel>
                 <div>
                     {forms}
                 </div>
